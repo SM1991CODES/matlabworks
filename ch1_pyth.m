5bef69b9fadda6ab41c003b90d8b9c6f24c6b284
sambit <sambit.mohapatra1903@gmail.com>
2023-07-02 10:47:22 +0200

    - sync

--- ch1_pyth.m
+++ ch1_pyth.m
@@ -5,3 +5,11 @@ alpha = -pi/2: pi/100: pi/2;
 F_alpha = 1 - (sin(alpha) .^ 2 + cos(alpha) .^ 2);
 
 disp(F_alpha)
+
+% find values of alpha that gave F(alpha) = 0
+alpha_zeros = alpha(F_alpha == 0)
+
+% plot alpha vs F(alpha)
+plot(alpha, F_alpha)
+xlabel('\alpha')  % \alpha allows greek letter alpha
+ylabel('F(\alpha)')
